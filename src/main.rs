255d196878cbba81f8b728ce8b27a1341e6b28bf
Laurentiu Nicola <lnicola@dend.ro>
2018-03-28 14:01:52 +0300

    Clean up code, add timeouts, use failure crate

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,37 @@
-#![feature(alloc_system)]
+#![feature(alloc_system, global_allocator, allocator_api)]
 
+extern crate alloc_system;
 extern crate bytes;
+#[macro_use]
+extern crate failure;
 extern crate futures;
+extern crate hyper;
 extern crate tokio_core;
 extern crate tokio_io;
+extern crate tokio_serial;
 extern crate tokio_service;
 extern crate tokio_timer;
-extern crate tokio_serial;
-extern crate hyper;
-extern crate alloc_system;
 
+use alloc_system::System;
 use bytes::{BufMut, BytesMut};
-use std::{io, env, str};
+use futures::{future, Future, Sink, Stream};
+use hyper::client::Request;
+use hyper::header::{Connection, ContentLength, ContentType};
+use hyper::{Client, Method, Uri};
 use std::path::PathBuf;
-use std::time::{self, Instant, Duration, SystemTime};
-use std::sync::Arc;
 use std::str::FromStr;
-use futures::{Future, Stream, Sink};
+use std::sync::Arc;
+use std::time::{self, Duration, Instant, SystemTime};
+use std::{env, io, str};
 use tokio_core::reactor::{Core, Handle};
 use tokio_io::AsyncRead;
 use tokio_io::codec::{Decoder, Encoder};
-use tokio_service::Service;
-use tokio_timer::Timer;
 use tokio_serial::{Serial, SerialPortSettings};
-use hyper::{Client, Method, Uri};
-use hyper::client::Request;
-use hyper::header::{Connection, ContentLength, ContentType};
+use tokio_service::Service;
+use tokio_timer::{TimeoutError, Timer, TimerError};
+
+#[global_allocator]
+static A: System = System;
 
 enum SensorCommand {
     Measure,
@@ -46,23 +52,19 @@ impl Decoder for SensorCodec {
         let newline = src.as_ref().iter().position(|b| *b == b'\n');
         if let Some(n) = newline {
             let line = src.split_to(n + 1);
-            let r = str::from_utf8(line.as_ref())
-                .ok()
-                .and_then(|s| {
-                    use std::str::FromStr;
-                    let mut it = s.split_whitespace();
-                    let humidity = it.next().and_then(|s| f32::from_str(s).ok());
-                    let temperature = it.next().and_then(|s| f32::from_str(s).ok());
-                    match (humidity, temperature) {
-                        (Some(humidity), Some(temperature)) => {
-                            Some(SensorReading {
-                                temperature: temperature,
-                                humidity: humidity,
-                            })
-                        }
-                        (_, _) => None,
-                    }
-                });
+            let r = str::from_utf8(line.as_ref()).ok().and_then(|s| {
+                use std::str::FromStr;
+                let mut it = s.split_whitespace();
+                let humidity = it.next().and_then(|s| f32::from_str(s).ok());
+                let temperature = it.next().and_then(|s| f32::from_str(s).ok());
+                match (humidity, temperature) {
+                    (Some(humidity), Some(temperature)) => Some(SensorReading {
+                        temperature,
+                        humidity,
+                    }),
+                    (_, _) => None,
+                }
+            });
             return match r {
                 Some(r) => Ok(Some(r)),
                 _ => Err(io::Error::new(io::ErrorKind::Other, "Invalid string")),
@@ -93,24 +95,39 @@ struct Sensor {
     serial_settings: SerialPortSettings,
 }
 
+#[derive(Debug, Fail)]
+enum Error {
+    #[fail(display = "an IO error has occurred: {}", _0)]
+    Io(io::Error),
+    #[fail(display = "an HTTP error has occurred: {}", _0)]
+    Hyper(hyper::Error),
+    #[fail(display = "an timer error has occurred: {}", _0)]
+    Timer(TimerError),
+    #[fail(display = "an operation has timed out")]
+    Timeout,
+}
+
 impl Service for Sensor {
     type Request = SensorCommand;
     type Response = SensorReading;
-    type Error = io::Error;
+    type Error = Error;
 
     type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
 
     fn call(&self, req: Self::Request) -> Self::Future {
-        let port = Serial::from_path(&self.path, &self.serial_settings, &self.handle).unwrap();
-        let transport = port.framed(SensorCodec);
+        let serial = Serial::from_path(&self.path, &self.serial_settings, &self.handle);
 
-        Box::new(transport.send(req)
-            .and_then(|transport| transport.into_future().map_err(|(e, _)| e))
-            .and_then(|(reading, _)| match reading {
-                Some(r) => Ok(r),
-                _ => Err(io::Error::new(io::ErrorKind::Other, "Read failed")),
-            })
-            )
+        Box::new(
+            future::result(serial)
+                .map(|port| port.framed(SensorCodec))
+                .and_then(|transport| transport.send(req))
+                .and_then(|transport| transport.into_future().map_err(|(e, _)| e))
+                .and_then(|(reading, _)| match reading {
+                    Some(r) => Ok(r),
+                    _ => Err(io::Error::new(io::ErrorKind::Other, "Read failed")),
+                })
+                .map_err(Error::Io),
+        )
     }
 }
 
@@ -128,17 +145,19 @@ struct Influx {
 impl Service for Influx {
     type Request = InfluxData;
     type Response = hyper::Response;
-    type Error = hyper::Error;
+    type Error = Error;
 
     type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
 
     fn call(&self, req: Self::Request) -> Self::Future {
-        let timestamp = req.timestamp.duration_since(time::UNIX_EPOCH).unwrap().as_secs();
-        let msg = format!("temperature,host=ubik value={} {}\nhumidity,host=ubik value={} {}\n",
-                          req.temperature,
-                          timestamp,
-                          req.humidity,
-                          timestamp);
+        let timestamp = req.timestamp
+            .duration_since(time::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        let msg = format!(
+            "temperature,host=ubik value={} {}\nhumidity,host=ubik value={} {}\n",
+            req.temperature, timestamp, req.humidity, timestamp
+        );
         let client = Client::new(&self.handle);
         let mut request = Request::new(Method::Post, self.url.clone());
         {
@@ -149,15 +168,15 @@ impl Service for Influx {
         }
         request.set_body(msg);
 
-        Box::new(client.request(request))
+        Box::new(client.request(request).map_err(Error::Hyper))
     }
 }
 
 fn main() {
     let mut args = env::args();
     let tty_path = args.nth(1).unwrap_or_else(|| String::from("/dev/ttyACM0"));
-    let influx_url = Uri::from_str("http://127.0.0.1:8086/write?db=temperature&precision=s").unwrap();
-    println!("{}", influx_url);
+    let influx_url =
+        Uri::from_str("http://127.0.0.1:8086/write?db=temperature&precision=s").unwrap();
 
     let mut core = Core::new().unwrap();
     let handle = core.handle();
@@ -177,19 +196,26 @@ fn main() {
     let wakeups = timer.interval_at(Instant::now(), Duration::from_secs(10));
     let reads = wakeups.for_each(|_| {
         let influx = Arc::clone(&influx);
-        let handle2 = handle.clone();
-        let reading = sensor.call(SensorCommand::Measure)
-            .and_then(move |r| {
-                let data = InfluxData {
-                    temperature: r.temperature,
-                    humidity: r.humidity,
-                    timestamp: SystemTime::now(),
-                };
-                let sender = influx.call(data).map(|_| ()).map_err(|_| ());
-                handle2.spawn(sender);
-                Ok(())
+        let reading = sensor.call(SensorCommand::Measure).and_then(move |r| {
+            let data = InfluxData {
+                temperature: r.temperature,
+                humidity: r.humidity,
+                timestamp: SystemTime::now(),
+            };
+            influx.call(data).map(|r| {
+                if !r.status().is_success() {
+                    eprintln!("{:?}", r);
+                }
             })
-            .map_err(|_| ());
+        });
+        let reading = timer
+            .timeout(reading, Duration::from_secs(6))
+            .map_err(|e| match e {
+                TimeoutError::Timer(_, e) => Error::Timer(e),
+                TimeoutError::TimedOut(_) => Error::Timeout,
+                TimeoutError::Inner(e) => e,
+            })
+            .map_err(|e| eprintln!("{}", e));
 
         handle.spawn(reading);
         Ok(())
