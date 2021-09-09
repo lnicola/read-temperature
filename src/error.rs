849fb8e5d3f3d2a6564ab8647d417f01ccda2574
Laurențiu Nicola <lnicola@dend.ro>
2021-09-09 10:27:20 +0300

    Switch from Influx to Postgres

--- src/error.rs
+++ src/error.rs
@@ -1,21 +1,24 @@
-use hyper;
 use std::error;
 use std::fmt::{self, Display, Formatter};
 use std::io;
 
+use bb8::RunError;
+
 #[derive(Debug)]
 pub enum Error {
     Io(io::Error),
-    Hyper(hyper::Error),
     Serial(tokio_serial::Error),
+    Postgres(tokio_postgres::Error),
+    Timeout,
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
             Error::Io(e) => write!(f, "Input/Output error: {}.", e),
-            Error::Hyper(e) => write!(f, "HTTP error: {}.", e),
             Error::Serial(e) => write!(f, "Serial port error: {}.", e),
+            Error::Postgres(e) => write!(f, "Database error: {}.", e),
+            Error::Timeout => write!(f, "Timeout."),
         }
     }
 }
@@ -28,14 +31,23 @@ impl From<io::Error> for Error {
     }
 }
 
-impl From<hyper::Error> for Error {
-    fn from(err: hyper::Error) -> Self {
-        Error::Hyper(err)
-    }
-}
-
 impl From<tokio_serial::Error> for Error {
     fn from(err: tokio_serial::Error) -> Self {
         Error::Serial(err)
+    }
+}
+
+impl From<tokio_postgres::Error> for Error {
+    fn from(err: tokio_postgres::Error) -> Self {
+        Error::Postgres(err)
+    }
+}
+
+impl From<RunError<tokio_postgres::Error>> for Error {
+    fn from(err: RunError<tokio_postgres::Error>) -> Self {
+        match err {
+            RunError::User(err) => Error::Postgres(err),
+            RunError::TimedOut => Error::Timeout,
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
+use bb8::Pool;
+use bb8_postgres::PostgresConnectionManager;
 use bytes::{BufMut, BytesMut};
+use chrono::{DateTime, SubsecRound, Utc};
 use error::Error;
-use futures::future::TryFutureExt;
 use futures::sink::SinkExt;
 use futures::stream::StreamExt;
-use hyper;
-use hyper::client::HttpConnector;
-use hyper::header::CONTENT_TYPE;
-use hyper::{Body, Client, Request, Response, Uri};
 use std::io;
 use std::str::FromStr;
-use std::sync::Arc;
 use std::time::Duration;
 use std::{env, str};
 use tokio::runtime::Runtime;
+use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
 use tokio::time::{self, Instant};
+use tokio_postgres::{Config, NoTls};
 use tokio_serial::SerialPortBuilderExt;
 use tokio_util::codec::{Decoder, Encoder};
 
@@ -92,66 +91,113 @@ impl Sensor {
     }
 }
 
-struct Influx {
-    url: Uri,
-    client: Client<HttpConnector>,
-}
-
-impl Influx {
-    async fn call(&self, message: String) -> Result<Response<Body>, Error> {
-        let request = Request::post(&self.url)
-            .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
-            .body(Body::from(message))
-            .unwrap();
-        let response = self.client.request(request).await?;
-        Ok(response)
-    }
-}
-
 async fn co2_thread(
-    influx: Arc<Influx>,
     sensor: co2mon::Sensor,
+    tx: UnboundedSender<Reading>,
 ) -> Result<(), Box<dyn std::error::Error + Send>> {
     let mut interval = time::interval_at(Instant::now(), Duration::from_secs(10));
     loop {
         interval.tick().await;
         match sensor.read() {
             Ok(reading) => {
-                let msg = format!(
-                    "temperature2,host=ubik value={}\nco2,host=ubik value={}\n",
-                    reading.temperature(),
-                    reading.co2()
-                );
-                let influx = influx.clone();
-                let f = async move {
-                    let r = influx.call(msg).await?;
-                    if !r.status().is_success() {
-                        eprintln!("{:?}", r);
-                    }
-                    Ok::<(), Error>(())
-                }
-                .map_err(|e| eprintln!("{}", e));
-                tokio::spawn(f);
+                let time = Utc::now().round_subsecs(0);
+                let reading = Reading::Co2Meter {
+                    time,
+                    temperature: reading.temperature(),
+                    co2: reading.co2(),
+                };
+                tx.send(reading).unwrap();
             }
             Err(e) => eprintln!("{}", e),
         }
     }
 }
 
+#[derive(Debug)]
+enum Reading {
+    Thermometer {
+        time: DateTime<Utc>,
+        temperature: f32,
+        humidity: f32,
+    },
+    Co2Meter {
+        time: DateTime<Utc>,
+        temperature: f32,
+        co2: u16,
+    },
+}
+
+async fn send_one(
+    pool: &Pool<PostgresConnectionManager<NoTls>>,
+    reading: Reading,
+) -> Result<(), Error> {
+    let conn = pool.get().await?;
+    match reading {
+        Reading::Thermometer {
+            time,
+            temperature,
+            humidity,
+        } => {
+            let temperature = (temperature * 100.0).round() as i16;
+            let humidity = (humidity * 100.0).round() as i16;
+            conn.query(
+                "insert into stats (time, temperature, humidity) values ($1, $2, $3);",
+                &[&time.naive_utc(), &temperature, &humidity],
+            )
+            .await?;
+        }
+        Reading::Co2Meter {
+            time,
+            temperature,
+            co2,
+        } => {
+            let temperature = (temperature * 100.0).round() as i16;
+            let co2 = co2 as i16;
+            conn.query(
+                "insert into stats2 (time, temperature, co2) values ($1, $2, $3);",
+                &[&time.naive_utc(), &temperature, &co2],
+            )
+            .await?;
+        }
+    }
+    Ok(())
+}
+
+async fn db_thread(
+    mut rx: UnboundedReceiver<Reading>,
+    pool: Pool<PostgresConnectionManager<NoTls>>,
+) {
+    while let Some(reading) = rx.recv().await {
+        if let Err(e) = send_one(&pool, reading).await {
+            eprintln!("{}", e);
+        }
+    }
+}
+
 async fn run(tty_path: String) {
-    let url = Uri::from_str("http://127.0.0.1:8086/write?db=temperature&precision=s").unwrap();
     let temperature_sensor = Sensor {
         path: tty_path,
         baud_rate: 9600,
     };
-    let client = Client::new();
-    let influx = Influx { url, client };
-    let influx = Arc::new(influx);
+    let mut config = Config::new();
 
-    let influx_ = influx.clone();
+    let host = env::var("DB_HOST").unwrap();
+    let user = env::var("DB_USER").unwrap();
+    let pass = env::var("DB_PASS").unwrap();
+    let name = env::var("DB_NAME").unwrap();
+    config.host(&host).user(&user).password(&pass).dbname(&name);
+    let (tx, rx) = mpsc::unbounded_channel();
+    let tx_ = tx.clone();
+    let manager = PostgresConnectionManager::new(config, NoTls);
+    let pool = Pool::builder().max_size(1).build(manager).await.unwrap();
+
+    tokio::spawn(async move {
+        db_thread(rx, pool).await;
+    });
+
     match co2mon::Sensor::open_default() {
         Ok(sensor) => {
-            tokio::spawn(co2_thread(influx_, sensor));
+            tokio::spawn(co2_thread(sensor, tx_));
         }
         Err(e) => {
             eprintln!("{}", e);
@@ -162,17 +208,16 @@ async fn run(tty_path: String) {
     loop {
         interval.tick().await;
         let temperature_sensor = temperature_sensor.clone();
-        let influx = Arc::clone(&influx);
+        let tx = tx.clone();
         let one = async move {
             let reading = temperature_sensor.call(SensorCommand::Measure).await?;
-            let msg = format!(
-                "temperature,host=ubik value={}\nhumidity,host=ubik value={}\n",
-                reading.temperature, reading.humidity
-            );
-            let r = influx.call(msg).await?;
-            if !r.status().is_success() {
-                eprintln!("{:?}", r);
-            }
+            let time = Utc::now().round_subsecs(0);
+            let reading = Reading::Thermometer {
+                time,
+                temperature: reading.temperature,
+                humidity: reading.humidity,
+            };
+            tx.send(reading).unwrap();
             Ok::<(), Error>(())
         };
         if let Err(e) = time::timeout(Duration::from_secs(6), one).await {
@@ -184,11 +229,7 @@ async fn run(tty_path: String) {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut args = env::args();
     let arg = args.nth(1);
-    let tty_path = arg
-        .as_ref()
-        .map(String::as_str)
-        .unwrap_or("/dev/ttyACM0")
-        .to_string();
+    let tty_path = arg.as_deref().unwrap_or("/dev/ttyACM0").to_string();
 
     let rt = Runtime::new()?;
     rt.block_on(async move { run(tty_path).await });
